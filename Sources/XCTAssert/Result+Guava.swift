d0f7cfd704e94bfd5a011ff7ae61b0d9925132f9
Denis Chagin <chaginden@gmail.com>
2020-05-19 19:53:14 +0300

    xctassert: Extend Result with Failure
    - Add `reportFailure` method that sends report to `FailureReporter`
      if result is `failure`.

---
+++ Sources/XCTAssert/Result+Guava.swift
@@ -0,0 +1,12 @@
+extension Result where Failure == Guava.Failure {
+
+    /// Sends report to `FailureReporter` if result is `failure`.
+    /// - Parameters:
+    ///   - file: The file in which failure occurred. Defaults to the file name of the test case in which this function was called.
+    ///   - line: The line number on which failure occurred. Defaults to the line number on which this function was called.
+    func reportFailure(file: StaticString, line: UInt) {
+        if case .failure(let failure) = self {
+            FailureReporter.handler.handleFailure(failure, location: .init(file: file, line: line))
+        }
+    }
+}
