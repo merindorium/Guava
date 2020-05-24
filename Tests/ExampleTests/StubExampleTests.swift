86da148a580d2bc73d6d330d1a06adfbe1ee7f35
Denis Chagin <chaginden@gmail.com>
2020-05-25 00:41:57 +0300

    tests: Add ExampleTest directory with tests examples for documentation

---
+++ Tests/ExampleTests/StubExampleTests.swift
@@ -0,0 +1,55 @@
+import Guava
+import XCTest
+
+protocol TipStorage {
+
+    func fetchAllTips() -> [Int]
+}
+
+final class TipCalculator {
+
+    private let tipStorage: TipStorage
+
+    init(tipStorage: TipStorage) {
+        self.tipStorage = tipStorage
+    }
+
+    func sumAllTips() -> Int {
+        return tipStorage.fetchAllTips().reduce(0, +)
+    }
+}
+
+final class TipStorageTestDouble: TipStorage {
+
+    let fetchAllTipsMethod = TestDoubleFactory<[Int]>()
+
+    func fetchAllTips() -> [Int] {
+        return fetchAllTipsMethod.invoke(arguments: [])
+    }
+}
+
+final class TipCalculatorTests: XCTestCase {
+
+    func testAllTipsAreSummedCorrectly() {
+        // Prepare test fixtures
+        let lastTips = [5, 5, 10]
+        let expectedTipsSum = 20
+
+        // Arrange SUT and its dependencies
+        let tipStorageTestDouble = TipStorageTestDouble()
+        let tipCalculator = TipCalculator(tipStorage: tipStorageTestDouble)
+
+        // Set up test double as `Stub` for this test case
+        tipStorageTestDouble.fetchAllTipsMethod.stub(lastTips)
+
+        // Act
+        let tipsSum = tipCalculator.sumAllTips()
+
+        // Assert
+        XCTAssertEqual(tipsSum, expectedTipsSum)
+    }
+
+    static var allTests = [
+        ("testAllTipsAreSummedCorrectly", testAllTipsAreSummedCorrectly)
+    ]
+}
---
+++ Tests/ExampleTests/TestDoubleFactoryExample.swift
@@ -0,0 +1,43 @@
+import Guava
+import XCTest
+
+protocol Calculator {
+
+    var manufacturer: String { get }
+
+    func multiply(_ lhs: Int, _ rhs: Int) -> Int
+}
+
+final class CalculatorTestDouble {
+
+    let manufacturerProp = TestDoubleFactory<String>()
+    let multiplyMethod = TestDoubleFactory<Int>()
+}
+
+extension CalculatorTestDouble: Calculator {
+
+    var manufacturer: String {
+        return manufacturerProp.invoke(arguments: [])
+    }
+
+    func multiply(_ lhs: Int, _ rhs: Int) -> Int {
+        return multiplyMethod.invoke(arguments: [lhs, rhs])
+    }
+}
+
+final class CalculatorTestCase: XCTestCase {
+
+    func testMultiply() {
+        let stubValue = 5
+        let calculatorTestDouble = CalculatorTestDouble()
+
+        calculatorTestDouble.multiplyMethod.stub(stubValue)
+
+        let result = calculatorTestDouble.multiply(3, 3)
+        XCTAssertEqual(result, stubValue)
+    }
+
+    static var allTests = [
+        ("testMultiply", testMultiply)
+    ]
+}
