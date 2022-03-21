d264e9a09339f69936abeffaaa538bb783d88401
Terky <artemab@outlook.com>
2022-03-21 14:57:32 +0300

    entities: Update Fake to conform to ThrowingInvokable

--- Sources/Entities/Fake.swift
+++ Sources/Entities/Fake.swift
@@ -1,19 +1,43 @@
 /// A `Fake` allows to swap testable entity implementation with stored one.
 final class Fake<Value> {
 
-    private let closure: ([Argument]) -> Value
+    private let closure: Closure<Value>
 
     init(_ fakeClosure: @escaping ([Argument]) -> Value) {
-        closure = fakeClosure
+        closure = .normal(fakeClosure)
+    }
+
+    init(_ fakeThrowingClosure: @escaping ([Argument]) throws -> Value) {
+        closure = .throwing(fakeThrowingClosure)
     }
 }
 
 extension Fake: Invokable {
 
     func invoke(arguments: [Any]) -> Value {
+        guard case .normal(let closure) = closure else {
+            FailureReporter
+                .handler
+                .handleFatalError(.testDoubleTypeMismatch(expected: "Normal", received: "Throwing"), location: nil)
+        }
         let arguments = arguments
             .enumerated()
             .map { Argument(value: $0.element, position: .init($0.offset)) }
         return closure(arguments)
+    }
+}
+
+extension Fake: ThrowingInvokable {
+
+    func throwingInvoke(arguments: [Any]) throws -> Value {
+        guard case .throwing(let closure) = closure else {
+            FailureReporter
+                .handler
+                .handleFatalError(.testDoubleTypeMismatch(expected: "Throwing", received: "Normal"), location: nil)
+        }
+        let arguments = arguments
+            .enumerated()
+            .map { Argument(value: $0.element, position: .init($0.offset)) }
+        return try closure(arguments)
     }
 }
