98df5787afa9409ebefe0b89f58a7c202cb71143
Terky <artemab@outlook.com>
2022-03-21 14:58:01 +0300

    entities: Update Stub to conform to ThrowingInvokable

--- Sources/Entities/Stub.swift
+++ Sources/Entities/Stub.swift
@@ -1,16 +1,38 @@
 /// A `Stub` provides hardcoded answers to calls made during the test.
 final class Stub<Value> {
 
-    private var stubbedValue: Value
+    private var stubbedValue: Result<Value, Error>
 
     init(stubbedValue: Value) {
-        self.stubbedValue = stubbedValue
+        self.stubbedValue = .success(stubbedValue)
+    }
+
+    init(error: Error) {
+        stubbedValue = .failure(error)
     }
 }
 
 extension Stub: Invokable {
 
     func invoke(arguments: [Any]) -> Value {
-        return stubbedValue
+        guard case .success(let result) = stubbedValue else {
+            FailureReporter
+                .handler
+                .handleFatalError(.testDoubleTypeMismatch(expected: "Normal", received: "Throwing"), location: nil)
+        }
+
+        return result
+    }
+}
+
+extension Stub: ThrowingInvokable {
+
+    func throwingInvoke(arguments: [Any]) throws -> Value {
+        switch stubbedValue {
+        case .success(let value):
+            return value
+        case .failure(let error):
+            throw error
+        }
     }
 }
