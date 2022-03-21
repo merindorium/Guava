3934db4272a87474a03a95a4bfb0ccadfa3fb213
Terky <artemab@outlook.com>
2022-03-21 14:58:23 +0300

    entities: Update Spy to conform to ThrowingInvokable

--- Sources/Entities/Spy.swift
+++ Sources/Entities/Spy.swift
@@ -8,6 +8,10 @@ public final class Spy<Value> {
     public init(value: Value) {
         stub = Stub(stubbedValue: value)
     }
+
+    public init(error: Error) {
+        stub = Stub(error: error)
+    }
 }
 
 extension Spy: Invokable {
@@ -16,5 +20,14 @@ extension Spy: Invokable {
         calls.append(RecordedMethodCall(arguments: arguments))
 
         return stub.invoke(arguments: arguments)
+    }
+}
+
+extension Spy: ThrowingInvokable {
+
+    func throwingInvoke(arguments: [Any]) throws -> Value {
+        calls.append(RecordedMethodCall(arguments: arguments))
+
+        return try stub.throwingInvoke(arguments: arguments)
     }
 }
