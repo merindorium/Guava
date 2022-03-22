970764594cf9b12048507cb3410ec870be2fadb5
Terky <artemab@outlook.com>
2022-03-22 12:55:22 +0300

    entities: Update Closure to support async closures and add description

--- Sources/Entities/Closure.swift
+++ Sources/Entities/Closure.swift
@@ -2,4 +2,19 @@ enum Closure<Value> {
 
     case normal(([Argument]) -> Value)
     case throwing(([Argument]) throws -> Value)
+    case async(([Argument]) async -> Value)
+}
+
+extension Closure: CustomStringConvertible {
+
+    var description: String {
+        switch self {
+        case .normal:
+            return "Normal"
+        case .throwing:
+            return "Throwing"
+        case .async:
+            return "Async"
+        }
+    }
 }
