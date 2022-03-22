0789fc468472be893985989ee802b1bd2606a3dd
Terky <artemab@outlook.com>
2022-03-22 13:11:12 +0300

    entities: Add AsyncTestDoubleFactory

---
+++ Sources/Entities/AsyncTestDoubleFactory.swift
@@ -0,0 +1,51 @@
+/// A `AsyncTestDoubleFactory` is a factory that allows to swap testable entity with several kinds of throwing doubles.
+@available(iOS 13, macOS 10.15, *)
+public final class AsyncTestDoubleFactory<Value> {
+
+    private var invokeClosure: (([Any]) async -> Value)?
+
+    public init() {}
+}
+
+@available(iOS 13, macOS 10.15, *)
+extension AsyncTestDoubleFactory: AsyncInvokable {
+
+    public func asyncInvoke(arguments: [Any]) async -> Value {
+        guard let closure = invokeClosure else {
+            FailureReporter.handler.handleFatalError(.nilValue, location: nil)
+        }
+
+        return await closure(arguments)
+    }
+}
+
+@available(iOS 13, macOS 10.15, *)
+extension AsyncTestDoubleFactory {
+
+    /// Creates `Stub`.
+    /// - Parameter value: Stub value.
+    /// - Parameter delayInNanoseconds: amount of nanoseconds to wait before returning a result.
+    public func stub(_ value: Value, delayInNanoseconds: UInt64 = .zero) {
+        let stub = Stub(stubbedValue: value, delayInNanoseconds: delayInNanoseconds)
+
+        invokeClosure = stub.asyncInvoke(arguments:)
+    }
+
+    /// Returns `Spy`.
+    /// - Parameter value: Stub value.
+    /// - Parameter delayInNanoseconds: amount of nanoseconds to wait before returning a result.
+    public func spy(_ value: Value, delayInNanoseconds: UInt64 = .zero) -> Spy<Value> {
+        let spy = Spy(value: value, delayInNanoseconds: delayInNanoseconds)
+
+        invokeClosure = spy.asyncInvoke(arguments:)
+
+        return spy
+    }
+
+    /// Creates `Fake`.
+    /// - Parameter closure: Async closure that will swap actual implementation of entity.
+    public func fake(_ closure: @escaping ([Argument]) async -> Value) {
+        let fake = Fake(closure)
+        invokeClosure = fake.asyncInvoke(arguments:)
+    }
+}
