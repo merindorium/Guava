ef1e31f6732b22529d11df0d32161f58e2fdcf8b
Terky <artemab@outlook.com>
2022-03-28 17:37:25 +0300

    entities: Add AsyncStub

---
+++ Sources/Entities/AsyncStub.swift
@@ -0,0 +1,57 @@
+/// An `AsyncStub` provides hardcoded answers to calls made during the test.
+@available(iOS 13, macOS 10.15, *)
+actor AsyncStub<Value> {
+
+    private let stubbedValue: Result<Value, Error>
+    private let delayInNanoseconds: UInt64
+
+    init(stubbedValue: Value, delayInNanoseconds: UInt64 = .zero) {
+        self.stubbedValue = .success(stubbedValue)
+        self.delayInNanoseconds = delayInNanoseconds
+    }
+
+    init(error: Error, delayInNanoseconds: UInt64 = .zero) {
+        stubbedValue = .failure(error)
+        self.delayInNanoseconds = delayInNanoseconds
+    }
+}
+
+@available(iOS 13, macOS 10.15, *)
+extension AsyncStub: AsyncInvokable {
+
+    @Sendable
+    func asyncInvoke(arguments: [Any]) async -> Value {
+        guard case .success(let result) = stubbedValue else {
+            FailureReporter
+                .handler
+                .handleFatalError(.testDoubleTypeMismatch(expected: "Async", received: "Throwing"), location: nil)
+        }
+
+        do {
+            try await Task.sleep(nanoseconds: delayInNanoseconds)
+        } catch {
+            FailureReporter.handler.handleFatalError(.taskExplicitlyCanceled, location: nil)
+        }
+        return result
+    }
+}
+
+@available(iOS 13, macOS 10.15, *)
+extension AsyncStub: ThrowingAsyncInvokable {
+
+    @Sendable
+    func throwingAsyncInvoke(arguments: [Any]) async throws -> Value {
+        do {
+            try await Task.sleep(nanoseconds: delayInNanoseconds)
+        } catch {
+            FailureReporter.handler.handleFatalError(.taskExplicitlyCanceled, location: nil)
+        }
+
+        switch stubbedValue {
+        case .success(let value):
+            return value
+        case .failure(let error):
+            throw error
+        }
+    }
+}
