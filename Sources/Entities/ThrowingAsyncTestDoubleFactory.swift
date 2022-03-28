6ec73e950f562e57068945771449016a1425eb58
Terky <artemab@outlook.com>
2022-03-28 17:40:48 +0300

    entities: Update async factories to use async entities

--- Sources/Entities/ThrowingAsyncTestDoubleFactory.swift
+++ Sources/Entities/ThrowingAsyncTestDoubleFactory.swift
@@ -1,8 +1,8 @@
 /// A `AsyncTestDoubleFactory` is a factory that allows to swap testable entity with several kinds of throwing doubles.
 @available(iOS 13, macOS 10.15, *)
-public final class ThrowingAsyncTestDoubleFactory<Value> {
+public actor ThrowingAsyncTestDoubleFactory<Value> {
 
-    private var invokeClosure: (([Any]) async throws -> Value)?
+    private var invokeClosure: (@Sendable ([Any]) async throws -> Value)?
 
     public init() {}
 }
@@ -26,7 +26,7 @@ extension ThrowingAsyncTestDoubleFactory {
     /// - Parameter value: Stub value.
     /// - Parameter delayInNanoseconds: amount of nanoseconds to wait before returning a result.
     public func stub(_ value: Value, delayInNanoseconds: UInt64 = .zero) {
-        let stub = Stub(stubbedValue: value, delayInNanoseconds: delayInNanoseconds)
+        let stub = AsyncStub(stubbedValue: value, delayInNanoseconds: delayInNanoseconds)
 
         invokeClosure = stub.throwingAsyncInvoke(arguments:)
     }
@@ -35,7 +35,7 @@ extension ThrowingAsyncTestDoubleFactory {
     /// - Parameter error: Stub error to throw.
     /// - Parameter delayInNanoseconds: amount of nanoseconds to wait before returning a result.
     public func stub(_ error: Error, delayInNanoseconds: UInt64 = .zero) {
-        let stub = Stub<Value>(error: error, delayInNanoseconds: delayInNanoseconds)
+        let stub = AsyncStub<Value>(error: error, delayInNanoseconds: delayInNanoseconds)
 
         invokeClosure = stub.throwingAsyncInvoke(arguments:)
     }
@@ -43,8 +43,8 @@ extension ThrowingAsyncTestDoubleFactory {
     /// Returns `Spy`.
     /// - Parameter value: Stub value.
     /// - Parameter delayInNanoseconds: amount of nanoseconds to wait before returning a result.
-    public func spy(_ value: Value, delayInNanoseconds: UInt64 = .zero) -> Spy<Value> {
-        let spy = Spy(value: value, delayInNanoseconds: delayInNanoseconds)
+    public func spy(_ value: Value, delayInNanoseconds: UInt64 = .zero) -> AsyncSpy<Value> {
+        let spy = AsyncSpy(value: value, delayInNanoseconds: delayInNanoseconds)
 
         invokeClosure = spy.throwingAsyncInvoke(arguments:)
 
@@ -54,8 +54,8 @@ extension ThrowingAsyncTestDoubleFactory {
     /// Returns `Spy`.
     /// - Parameter error: Stub error to throw.
     /// - Parameter delayInNanoseconds: amount of nanoseconds to wait before returning a result.
-    public func spy(_ error: Error, delayInNanoseconds: UInt64 = .zero) -> Spy<Value> {
-        let spy = Spy<Value>(error: error, delayInNanoseconds: delayInNanoseconds)
+    public func spy(_ error: Error, delayInNanoseconds: UInt64 = .zero) -> AsyncSpy<Value> {
+        let spy = AsyncSpy<Value>(error: error, delayInNanoseconds: delayInNanoseconds)
 
         invokeClosure = spy.throwingAsyncInvoke(arguments:)
 
@@ -64,8 +64,8 @@ extension ThrowingAsyncTestDoubleFactory {
 
     /// Creates `Fake`.
     /// - Parameter closure: Throwing async closure that will swap actual implementation of entity.
-    public func fake(_ closure: @escaping ([Argument]) async throws -> Value) {
-        let fake = Fake(closure)
+    public func fake(_ closure: @escaping @Sendable ([Argument]) async throws -> Value) {
+        let fake = AsyncFake(closure)
         invokeClosure = fake.throwingAsyncInvoke(arguments:)
     }
 }
