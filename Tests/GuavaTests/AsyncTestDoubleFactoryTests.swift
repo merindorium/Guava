829c2f91e0e4dc8418d4dda3fc7e9269a35b90cf
Terky <artemab@outlook.com>
2022-03-28 17:49:21 +0300

    tests: Update async tests after async API update

--- Tests/GuavaTests/AsyncTestDoubleFactoryTests.swift
+++ Tests/GuavaTests/AsyncTestDoubleFactoryTests.swift
@@ -8,7 +8,7 @@ final class AsyncTestDoubleFactoryTests: XCTestCase {
         let multiplierTestDouble = AsyncMultiplierTestDouble()
         let calculator = AsyncCalculator(multiplier: multiplierTestDouble)
 
-        multiplierTestDouble.multiplyMethod.stub(5, delayInNanoseconds: 100_000_000)
+        await multiplierTestDouble.multiplyMethod.stub(5, delayInNanoseconds: 100_000_000)
 
         let result = await calculator.multiply(3, 3)
         XCTAssertEqual(result, 5)
@@ -18,38 +18,38 @@ final class AsyncTestDoubleFactoryTests: XCTestCase {
         let multiplierTestDouble = AsyncMultiplierTestDouble()
         let calculator = AsyncCalculator(multiplier: multiplierTestDouble)
 
-        let spy = multiplierTestDouble.multiplyMethod.spy(10, delayInNanoseconds: 100_000_000)
+        let spy = await multiplierTestDouble.multiplyMethod.spy(10, delayInNanoseconds: 100_000_000)
 
         let result = await calculator.multiply(3, 3)
         XCTAssertEqual(result, 10)
-        XCTAssertCalledOnce(spy)
+        await XCTAssertCalledOnce(spy)
     }
 
-    func testSpyNotCalledAsync() {
+    func testSpyNotCalledAsync() async {
         let multiplierTestDouble = AsyncMultiplierTestDouble()
         _ = AsyncCalculator(multiplier: multiplierTestDouble)
 
-        let spy = multiplierTestDouble.multiplyMethod.spy(10, delayInNanoseconds: 100_000_000)
+        let spy = await multiplierTestDouble.multiplyMethod.spy(10, delayInNanoseconds: 100_000_000)
 
-        XCTAssertNotCalled(spy)
+        await XCTAssertNotCalled(spy)
     }
 
     func testSpyCalledWithArgumentsAsync() async {
         let multiplierTestDouble = AsyncMultiplierTestDouble()
         let calculator = AsyncCalculator(multiplier: multiplierTestDouble)
 
-        let spy = multiplierTestDouble.multiplyMethod.spy(10, delayInNanoseconds: 100_000_000)
+        let spy = await multiplierTestDouble.multiplyMethod.spy(10, delayInNanoseconds: 100_000_000)
 
         _ = await calculator.multiply(3, 3)
 
-        XCTAssertCalled(spy, with: (3, 3))
+        await XCTAssertCalled(spy, with: (3, 3))
     }
 
     func testFakeAsync() async {
         let multiplierTestDouble = AsyncMultiplierTestDouble()
         let calculator = AsyncCalculator(multiplier: multiplierTestDouble)
 
-        multiplierTestDouble.multiplyMethod.fake { args in
+        await multiplierTestDouble.multiplyMethod.fake { args in
             do {
                 try await Task.sleep(nanoseconds: 100_000_000)
             } catch {
